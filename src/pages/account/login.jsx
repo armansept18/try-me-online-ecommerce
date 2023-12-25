73e6abb5893906bd4537ffb593892824853f07be
Arman Septian Pratama <armansept18@gmail.com>
2023-12-25 15:20:03 +0700

    pages: login register ui

--- src/pages/account/login.jsx
+++ src/pages/account/login.jsx
@@ -1,3 +1,104 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import LoginImage from "../../public/images/bg-tower.jpeg";
+import { Fingerprint, Visibility, VisibilityOff } from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
+
+const StyledLoginButton = styled(Button)({
+  backgroundColor: "#F6E6CD",
+  color: "#252525",
+  fontFamily: "Quicksand",
+  fontWeight: "700",
+  maxWidth: "125px",
+  width: "100vw",
+  "&:hover": {
+    backgroundColor: "#F37725",
+    color: "#F5F5F5",
+  },
+});
+
 export const LoginPage = () => {
-  return <></>;
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="flex justify-center items-center mt-6  mb-8">
+      <Card
+        sx={{ maxWidth: 568, maxHeight: 640, height: "100vh", width: "100vw" }}
+        className="flex flex-col justify-center items-center"
+      >
+        <CardMedia
+          component="img"
+          style={{ maxHeight: "300px" }}
+          image={LoginImage}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h4"
+            component="div"
+            style={{ fontFamily: "Quicksand", fontWeight: "500" }}
+          >
+            Keep in touch
+          </Typography>
+          <div className="flex flex-col max-w-md w-screen gap-4">
+            <TextField label="Email">Email</TextField>
+            <FormControl sx={{ m: 0, width: "47ch" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </div>
+          <div className="mt-8 mb-2">
+            <StyledLoginButton variant="contained">
+              Login <Fingerprint />
+            </StyledLoginButton>
+          </div>
+          <div>
+            <Button
+              style={{
+                color: "#F37725",
+                fontFamily: "Quicksand",
+                fontWeight: "400",
+              }}
+            >
+              Don't Have An Account?
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
--- src/pages/home/homepage.jsx
+++ src/pages/home/homepage.jsx
@@ -1,10 +1,14 @@
 import { Footer } from "../../components/footer/footer";
 import { Navbar } from "../../components/navbar/navbar";
+import { LoginPage } from "../account/login";
+import { RegisterPage } from "../account/register";
 
 export const Homepage = () => {
   return (
     <>
       <Navbar />
+      {/* <LoginPage /> */}
+      <RegisterPage />
       <Footer />
     </>
   );
