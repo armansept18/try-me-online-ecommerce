2836441464761c6b5def0fbdcb94af3928e26b3b
Arman Septian Pratama <armansept18@gmail.com>
2024-01-09 13:04:29 +0700

    feat: hide navbar when scroll

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -9,8 +9,10 @@ import {
   ImageList,
   Menu,
   MenuItem,
+  Slide,
   Toolbar,
   Tooltip,
+  useScrollTrigger,
 } from "@mui/material";
 import { AccountCircle, ShoppingCart } from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -19,6 +21,16 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { LogoutModal } from "../modal/logout";
 import { CartModal } from "../modal/cart";
+
+const HideOnScroll = ({ children }) => {
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+};
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
@@ -88,191 +100,193 @@ export const Navbar = () => {
   return (
     <>
       <CartModal open={openCartModal} onClose={() => setOpenCartModal(false)} />
-      <AppBar
-        position="fixed"
-        sx={{ backgroundColor: "transparent", boxShadow: "none" }}
-      >
-        <Container maxWidth="xl" sx={{ flexGrow: 1 }}>
-          <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNav}
-                color="inherit"
-              >
-                <MenuIcon sx={{ color: "#252525" }} />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorNav)}
-                onClose={handleCloseNav}
-                sx={{ display: { xs: "block", md: "none" } }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNav}>
-                    <StyledButton
-                      textAlign="center"
-                      component={Link}
-                      to={`/${page.toLowerCase()}`}
-                    >
-                      {page}
-                    </StyledButton>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <ImageList sx={{ display: { xs: "flex", md: "none" } }}>
-              <img
-                src={Logo}
-                alt=""
-                style={{
-                  maxWidth: "180px",
-                  maxHeight: "61px",
-                  width: "100vw",
-                  height: "100vh",
-                }}
-              />
-            </ImageList>
-
-            <ImageList sx={{ display: { xs: "none", md: "flex" } }}>
-              <img
-                src={Logo}
-                alt=""
-                style={{
-                  maxWidth: "180px",
-                  maxHeight: "61px",
-                  width: "100vw",
-                  height: "100vh",
-                }}
-              />
-            </ImageList>
-            <Box
-              className="flex items-center"
-              sx={{
-                flexGrow: 1,
-                display: { xs: "none", md: "flex" },
-                justifyContent: "flex-end",
-                marginRight: 5,
-                gap: 5,
-              }}
-            >
-              {pages.map((page) => (
-                <StyledButton
-                  key={page}
-                  onClick={handleCloseNav}
-                  sx={{ my: 2, color: "#252525", display: "block" }}
-                  component={Link}
-                  to={`/${page.toLowerCase()}`}
+      <HideOnScroll>
+        <AppBar
+          position="fixed"
+          sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+        >
+          <Container maxWidth="xl" sx={{ flexGrow: 1 }}>
+            <Toolbar disableGutters>
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNav}
+                  color="inherit"
                 >
-                  {page}
-                </StyledButton>
-              ))}
-            </Box>
-            {/* Icon */}
-            <LogoutModal
-              open={openLogoutModal}
-              onClose={() => setOpenLogoutModal(false)}
-            />
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                gap: "25px",
-              }}
-            >
-              <Box>
-                <IconButton aria-label="cart" onClick={handleCartClicked}>
-                  <StyledBadge
-                    badgeContent={totalQuantity || "0"}
-                    color="error"
-                    max={99}
-                  >
-                    <ShoppingCart fontSize="medium" />
-                  </StyledBadge>
+                  <MenuIcon sx={{ color: "#252525" }} />
                 </IconButton>
-              </Box>
-              <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open account menu">
-                  <IconButton
-                    size="large"
-                    aria-label="account-bar"
-                    aria-controls="account-bar"
-                    aria-haspopup="true"
-                    onClick={handleOpenUser}
-                    sx={{ p: 0 }}
-                  >
-                    {isLoggedIn ? (
-                      <AccountCircle fontSize="large" />
-                    ) : (
-                      <StyledButton
-                        component={Link}
-                        to="/login"
-                        sx={{ my: 2, color: "#252525", display: "block" }}
-                      >
-                        Login
-                      </StyledButton>
-                    )}
-                  </IconButton>
-                </Tooltip>
                 <Menu
-                  sx={{ mt: "45px" }}
-                  id="account-bar"
-                  anchorEl={anchorUser}
+                  id="menu-appbar"
+                  anchorEl={anchorNav}
                   anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
+                    vertical: "bottom",
+                    horizontal: "left",
                   }}
                   keepMounted
                   transformOrigin={{
                     vertical: "top",
-                    horizontal: "right",
+                    horizontal: "left",
                   }}
-                  open={Boolean(anchorUser)}
-                  onClose={handleCloseUser}
+                  open={Boolean(anchorNav)}
+                  onClose={handleCloseNav}
+                  sx={{ display: { xs: "block", md: "none" } }}
                 >
-                  {isLoggedIn ? (
-                    accountLoggedIn.map((item) => (
-                      <MenuItem key={item} onClick={handleCloseUser}>
-                        <StyledButton
-                          component={Link}
-                          to={`/${item.toLowerCase()}`}
-                        >
-                          {item}
-                        </StyledButton>
-                      </MenuItem>
-                    ))
-                  ) : (
-                    <MenuItem onClick={handleCloseUser}>
-                      <StyledButton component={Link} to="/login">
-                        Login
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNav}>
+                      <StyledButton
+                        textAlign="center"
+                        component={Link}
+                        to={`/${page.toLowerCase()}`}
+                      >
+                        {page}
                       </StyledButton>
                     </MenuItem>
-                  )}
-                  {isLoggedIn && (
-                    <MenuItem onClick={handleLogout}>
-                      <StyledButton>Logout</StyledButton>
-                    </MenuItem>
-                  )}
+                  ))}
                 </Menu>
               </Box>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+              <ImageList sx={{ display: { xs: "flex", md: "none" } }}>
+                <img
+                  src={Logo}
+                  alt=""
+                  style={{
+                    maxWidth: "180px",
+                    maxHeight: "61px",
+                    width: "100vw",
+                    height: "100vh",
+                  }}
+                />
+              </ImageList>
+
+              <ImageList sx={{ display: { xs: "none", md: "flex" } }}>
+                <img
+                  src={Logo}
+                  alt=""
+                  style={{
+                    maxWidth: "180px",
+                    maxHeight: "61px",
+                    width: "100vw",
+                    height: "100vh",
+                  }}
+                />
+              </ImageList>
+              <Box
+                className="flex items-center"
+                sx={{
+                  flexGrow: 1,
+                  display: { xs: "none", md: "flex" },
+                  justifyContent: "flex-end",
+                  marginRight: 5,
+                  gap: 5,
+                }}
+              >
+                {pages.map((page) => (
+                  <StyledButton
+                    key={page}
+                    onClick={handleCloseNav}
+                    sx={{ my: 2, color: "#252525", display: "block" }}
+                    component={Link}
+                    to={`/${page.toLowerCase()}`}
+                  >
+                    {page}
+                  </StyledButton>
+                ))}
+              </Box>
+              {/* Icon */}
+              <LogoutModal
+                open={openLogoutModal}
+                onClose={() => setOpenLogoutModal(false)}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "25px",
+                }}
+              >
+                <Box>
+                  <IconButton aria-label="cart" onClick={handleCartClicked}>
+                    <StyledBadge
+                      badgeContent={totalQuantity || "0"}
+                      color="error"
+                      max={99}
+                    >
+                      <ShoppingCart fontSize="medium" />
+                    </StyledBadge>
+                  </IconButton>
+                </Box>
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title="Open account menu">
+                    <IconButton
+                      size="large"
+                      aria-label="account-bar"
+                      aria-controls="account-bar"
+                      aria-haspopup="true"
+                      onClick={handleOpenUser}
+                      sx={{ p: 0 }}
+                    >
+                      {isLoggedIn ? (
+                        <AccountCircle fontSize="large" />
+                      ) : (
+                        <StyledButton
+                          component={Link}
+                          to="/login"
+                          sx={{ my: 2, color: "#252525", display: "block" }}
+                        >
+                          Login
+                        </StyledButton>
+                      )}
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    sx={{ mt: "45px" }}
+                    id="account-bar"
+                    anchorEl={anchorUser}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={Boolean(anchorUser)}
+                    onClose={handleCloseUser}
+                  >
+                    {isLoggedIn ? (
+                      accountLoggedIn.map((item) => (
+                        <MenuItem key={item} onClick={handleCloseUser}>
+                          <StyledButton
+                            component={Link}
+                            to={`/${item.toLowerCase()}`}
+                          >
+                            {item}
+                          </StyledButton>
+                        </MenuItem>
+                      ))
+                    ) : (
+                      <MenuItem onClick={handleCloseUser}>
+                        <StyledButton component={Link} to="/login">
+                          Login
+                        </StyledButton>
+                      </MenuItem>
+                    )}
+                    {isLoggedIn && (
+                      <MenuItem onClick={handleLogout}>
+                        <StyledButton>Logout</StyledButton>
+                      </MenuItem>
+                    )}
+                  </Menu>
+                </Box>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </HideOnScroll>
     </>
   );
 };
